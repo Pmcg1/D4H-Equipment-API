71960883c6b631b0b9cf9dcae8bcc156d1a77864
unknown <p.mcgowan1@gmail.com>
2020-11-29 00:26:15 +0000

    correctly accessing d4h api and returning a response

--- application.py
+++ application.py
@@ -1,7 +1,28 @@
 import requests
 import json
 
+def jsonformat(obj):
+    response = json.dumps(obj, indent=2)
+    print(response)
+
+
 from config import api_key
 
-url = https://api.d4h.org
+base_url = "https://api.d4h.org/v2"
+team  = "team"
+url = base_url+"/"+team
 
+
+print("Connecting to: ", url)
+print("API Key: ", api_key)
+
+response = requests.get(url, headers={'Authorization': 'Bearer %s' % api_key})
+print(response.status_code)
+print(response.iter_lines())
+
+response = requests.get(url+"/equipment", headers={'Authorization': 'Bearer %s' % api_key})
+print(response.status_code)
+print(response.iter_lines())
+
+jsonformat(response.json())
+#print(response)
